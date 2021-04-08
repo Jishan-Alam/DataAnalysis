2a00b53292cf43c5457268a12f2cc0f5487932ae
Jishan Alam <82015459+Jishan-Alam@users.noreply.github.com>
2021-04-08 14:40:10 +0530

    Delete ReadData.py

--- ReadData.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-data_set = pd.read_csv("D:/Data_QA/Test_Data/TestDataSet.csv")
-view_dataframe = data_set.head()
-
-t1 = data_set.tail()
-h1 = data_set.head()
-
-print(view_dataframe)
